0b5de5cf5c3319d9191ea1740aa27cd54f47aa99
Beatriz Salles <beatrizsallesss@gmail.com>
2021-10-26 15:47:45 -0300

    chore: Create sigin btton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
+import { SignInButton } from './SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
-      <h1>ig.news</h1>
       <div className={styles.headerContent}>
-        <img src="/images/logo.png" alt="Logo do ig.news" />
+        <img src="/images/logo.svg" alt="Logo do ig.news" />
       <nav>
         <a className={styles.active} href="#">Home</a>
         <a href="#">Posts</a>
       </nav>
+
+      <SignInButton />
       </div>
     </header>
   )
