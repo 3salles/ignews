db5cdcd8bd240b27d5a6cab1367a8869919d5bef
Beatriz Salles <beatrizsallesss@gmail.com>
2022-02-17 23:16:44 -0300

    feat: Add user to faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { query as faunaQuery } from "faunadb";
+
+import { fauna } from "../../..//services/fauna";
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,19 @@ export default NextAuth({
       // authorization: "https://github.com/login/oauth/authorize?scope=read:user+user:email"
     }),
   ],
-})
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          faunaQuery.Create(faunaQuery.Collection("users"), { data: { email } })
+        );
+  
+        return true;
+      } catch {
+        return false
+      }
+    },
+  },
+});
