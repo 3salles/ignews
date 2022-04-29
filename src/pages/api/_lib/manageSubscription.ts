f15d8540f5b462d93e8b8298f574ae2ddb13c751
Beatriz Salles <beatrizsallesss@gmail.com>
2022-04-29 11:56:28 -0300

    feat: Add Stripe webhooks

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,49 @@
+import { query as qFauna } from "faunadb";
+import { fauna } from "../../../services/fauna";
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false,
+) {
+  const userRef = await fauna.query(
+    qFauna.Select(
+      "ref",
+      qFauna.Get(
+        qFauna.Match(qFauna.Index("user_by_stripe_customer_id"), customerId)
+      )
+    )
+  );
+
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  };
+
+  if (createAction){
+    await fauna.query(
+      qFauna.Create(qFauna.Collection("subscriptions"), {
+        data: subscriptionData,
+      })
+    ); 
+  } else { 
+    await  fauna.query(
+      qFauna.Replace(
+        qFauna.Select('ref', 
+        qFauna.Get(
+          qFauna.Get(
+            qFauna.Match(
+              qFauna.Index('subscription_by_id'),
+              subscriptionId
+            )
+          )
+        )),
+        {data: subscriptionData}
+      )
+    )
+  }
+}
